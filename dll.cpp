3eea6cd3b20bd5dc5d82f10a6331706c8d008b52
Asti22 <uun3461@gmail.com>
2024-05-03 09:56:04 +0700

    membuat perintah untuk menyisipkan newnode di tengah data atau di akhir

--- dll.cpp
+++ dll.cpp
@@ -37,6 +37,30 @@ void addNode(){
     }
     else {
         //insert the new mode in the middle or at the end
+        Node*current = START; //step 1.a : start from the firts node
+        Node*previous = NULL; //step 1.b: previous node is NULL initially
+
+        while (current !=NULL && current->noMhs < newNode->noMhs)
+        {                          //step1.c: traverse the list to find the correct position
+            previous = current;     // step 1.d :move the previous to currect node
+            current = current -> next; //step 1.e : move the current to the next node
+        }
+
+        newNode->next =current; // step 4: make the next field of the new node point to current.
+        newNode-> prev =previous;   //step 5: make the previous of the new node point to previous .
+
+        if (current != NULL)
+        {
+            current->prev = newNode;   // step 6 : make rhe previous field of the current node point to the new node
+        }
+        if (previous != NULL)
+        {
+           previous->next =newNode; //step 7:make the next field of the previous node point to the new node
+        }
+        else{
+            //if previous is sti;; NULL, it means newNode is now the firts node
+            START =newNode;
+        }
     }
     
 }
