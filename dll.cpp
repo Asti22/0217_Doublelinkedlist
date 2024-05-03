84c992dfd13d2355f012b6c42f0ba4d385e4a31c
Asti22 <uun3461@gmail.com>
2024-05-03 11:08:41 +0700

    membuat prosdur void revtraverse

--- dll.cpp
+++ dll.cpp
@@ -154,3 +154,21 @@ void traverse()
     }
 }
 
+void revtraverse()
+{
+    if(listEmpty())
+    cout << "\nList is empty "<<endl;
+    else
+    {
+        cout <<"\nRecords in descending  orde of the roll number are: "<< endl;
+        Node*currentNode = START;
+        while(currentNode ->next != NULL)
+            currentNode = currentNode->next;
+        while (currentNode != NULL) 
+        {
+            cout << currentNode-> noMhs <<" "<<currentNode ->name <<endl;
+            currentNode = currentNode->prev;
+        }
+    }
+}
+
