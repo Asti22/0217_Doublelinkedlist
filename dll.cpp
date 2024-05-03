5fec552b2cfda84ca6ac6e747b878db13bd12adf
Asti22 <uun3461@gmail.com>
2024-05-03 11:18:42 +0700

    membuat prosedur seacrhdata untuk mencari data

--- dll.cpp
+++ dll.cpp
@@ -172,3 +172,23 @@ void revtraverse()
     }
 }
 
+void searchData()
+{
+    if (listEmpty()== true)
+    {
+        cout <<"\nList is empty "<<endl;
+
+    }
+    Node *prev, *curr;
+    prev = curr =NULL;
+    cout <<"\nEnter the roll number of the student whose record you want to seacrh:";
+    int num;
+    if (search(num,&prev, &curr)== false)
+       cout <<"\nRecord not found"<< endl;
+    else{
+        cout <<"\nRecord found"<<endl;
+        cout <<"\nRoll number: " <<curr ->noMhs <<endl;
+        cout <<"\nName: "<<curr->name <<endl;
+    }
+}
+
